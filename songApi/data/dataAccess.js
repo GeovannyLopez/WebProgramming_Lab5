52864c6590d3b8ce9d214e7af77c42b2be7ad95a
Oscar López <oscar.lopez@vivoconsulting.com>
2018-10-25 10:19:14 -0600

    Implementation of caché

--- songApi/data/dataAccess.js
+++ songApi/data/dataAccess.js
@@ -1,25 +1,63 @@
 let _ = require('lodash');
 let SongModel = require('../models/songModel');
 let connection = require('./connection');
+var redis = require('redis');
+
+var client = redis.createClient(); //creates a new client
 
 class dataAccess {
     getAll(callback) {
-        SongModel.find((err, songs) => {
-            if (err) throw err;
-            callback('success', songs)
+        //Check if the value exists in chaché
+        client.exists('songs_*', function(err, reply) {
+            //If the value exists
+            if (reply === 1) {
+                console.log('exists');
+
+                client.get('songs_*', function(error, result) {
+                    if (error) throw error;
+                    callback('success', JSON.parse(result));
+                  });
+            } else { //If the value doesn't exist
+                console.log('doesn\'t exist');
+
+                SongModel.find((err, songs) => {
+                    if (err) throw err;
+
+                    //Store the value in caché
+                    client.set('songs_*', JSON.stringify(songs), 'EX', 30);
+                    callback('success', songs);
+                });
+            }
         });
     }
 
     getSong(id, callback) {
-        SongModel.findOne({ _id: id }, (err, song) => {
-            console.log(song);
-            if (err) {
-                callback('error');
-            }else if (song === undefined || song === null){
-                callback('not found' );
-            }
-            else{
-                callback("success", song);
+        //Check if the value exists
+        client.exists('songs_' + id, function(err, reply) {
+            if (reply === 1) {
+                console.log('exists');
+
+                //Get the value from caché
+                client.get('songs_' + id, function(error, result) {
+                    if (error) throw error;
+                    callback('success', JSON.parse(result));
+                  });
+            } else { //If the values doesn't exist
+                console.log('doesn\'t exist');
+                
+                SongModel.findOne({ _id: id }, (err, song) => {
+                    console.log(song);
+                    if (err) {
+                        callback('error');
+                    }else if (!song){
+                        callback('not found');
+                    }
+                    else{
+                        //Store the value in caché
+                        client.set('songs_' + id, JSON.stringify(song), 'EX', 30);
+                        callback("success", song);
+                    }
+                });
             }
         });
     }
@@ -39,14 +77,14 @@ class dataAccess {
           });
           
           song.save();
-        return { status: 'success', song: song }
+        return { status: 'success', song }
     }
 
     editSong(id, editedSong, callback) {
         SongModel.findOne({ _id: id }, (err, song) => {
             if (err) {
                 callback('error');
-            }else if (song === undefined || song === null){
+            }else if (!song){
                 callback('not found');
             }
             else{
@@ -66,7 +104,7 @@ class dataAccess {
         SongModel.findOne({ _id: id }, (err, song) => {
             if (err) {
                 callback('error');
-            }else if (song === undefined || song === null){
+            }else if (!song){
                 callback('not found' );
             }
             else{
